9a4612725665237ed4a29830a7498e91807bf46d
jonnyz624 <tapiajonathan@gmail.com>
2024-08-12 15:46:51 -0500

    Enlaces

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+    import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { OfertasComponent } from './pages/ofertas/ofertas.component';
+import { Error404Component } from './pages/error404/error404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"home",component:HomeComponent},
+    {path:"nosotros",component:NosotrosComponent},
+    {path:"producto",component:ProductosComponent},
+    {path:"ofertas",component:OfertasComponent},
+
+    {path: '', redirectTo: 'home', pathMatch: 'full'}, 
+    {path:'**', component:Error404Component}
+];
--- src/app/components/nabvar/nabvar.component.ts
+++ src/app/components/nabvar/nabvar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nabvar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './nabvar.component.html',
   styleUrl: './nabvar.component.css'
 })
